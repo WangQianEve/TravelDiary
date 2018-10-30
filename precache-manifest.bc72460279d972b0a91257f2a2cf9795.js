864e16141792f6986c3902fa175e7c4dc80fcf6b
WangQianEve <eli_home@outlook.com>
2018-10-29 21:11:34 -0400

    Updates

---
+++ precache-manifest.bc72460279d972b0a91257f2a2cf9795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75e07557284ca0bc53e6",
+    "url": "/TravelDiary/static/js/runtime~main.75e07557.js"
+  },
+  {
+    "revision": "dd4a72445e03ca2775d5",
+    "url": "/TravelDiary/static/js/main.dd4a7244.chunk.js"
+  },
+  {
+    "revision": "625f5233f88851abddb3",
+    "url": "/TravelDiary/static/js/1.625f5233.chunk.js"
+  },
+  {
+    "revision": "dd4a72445e03ca2775d5",
+    "url": "/TravelDiary/static/css/main.e0a488ea.chunk.css"
+  },
+  {
+    "revision": "625f5233f88851abddb3",
+    "url": "/TravelDiary/static/css/1.9779074c.chunk.css"
+  },
+  {
+    "revision": "690c9ccc0e090277da3143af25113875",
+    "url": "/TravelDiary/index.html"
+  }
+];
